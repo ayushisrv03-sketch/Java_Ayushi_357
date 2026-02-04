03440c97d3ca1f1e66fd88f49f726bd41d019eba
ayushisrv03-sketch <ayushisrv03@gmail.com>
2026-02-04 13:03:54 +0530

    Jagged array

---
+++ jagged_array.java
@@ -0,0 +1,23 @@
+public class jagged_array {
+    public static void main(String[] args) {
+        int[][] jagged=new int[2][];
+        jagged[0]=new int[2];
+        jagged[1]=new int[3];
+
+        //assign value
+        jagged[0][0]=10;
+        jagged[0][1]=20;
+        jagged[1][0]=30;
+        jagged[1][1]=40;
+        jagged[1][2]=50;
+
+        for(int i=0;i<jagged.length;i++)
+        {
+            for(int j=0;j<jagged[i].length;j++)
+            {
+                System.out.print(jagged[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
