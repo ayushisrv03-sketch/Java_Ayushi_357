911bc73c0478ffe093e8e58ef4ff171fbc30a86d
ayushisrv03-sketch <ayushisrv03@gmail.com>
2026-02-04 11:04:55 +0530

    Factorial using recursion as well as iteration

--- fact.java
+++ fact.java
@@ -1,3 +1,22 @@
 public class fact {
-    
+    static int factrec(int n)
+    {
+        if(n==0 ||n==1)
+            {return 1;}
+        return n*factrec(n-1);
+    }
+    static int factiter(int n)
+    {
+        int result=1;
+        for(int i=1;i<=n;i++)
+        {
+            result=result*i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int number=5;
+        System.out.println("factorial of "+number+" is "+factrec(number));
+        System.out.println("factorial of "+number+" is "+factiter(number));
+    }
 }
